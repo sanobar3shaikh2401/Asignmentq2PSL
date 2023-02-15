1ee3269803711603319d1e72eb8de50235c8b7e6
sanobar3shaikh2401 <59875475+sanobar3shaikh2401@users.noreply.github.com>
2023-02-15 15:40:22 +0530

    Update Department.java

--- src/main/java/com/HQLProg/Question_2/Department.java
+++ src/main/java/com/HQLProg/Question_2/Department.java
@@ -19,7 +19,7 @@ public class Department {
 		DeptId = deptId;
 	}
 	public String getDeptName() {
-		return DeptName;
+		return abc;
 	}
 	public void setDeptName(String deptName) {
 		DeptName = deptName;
